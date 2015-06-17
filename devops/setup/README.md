7c12e831c1cdd7755cbd26c6bbe40614f3ac17de
Tyler Sanders <tyler.sanders@tech.geocent.com>
2015-06-17 12:20:56 -0500

    Docker setup script.

---
+++ devops/setup/README.md
@@ -0,0 +1,12 @@
+
+# Setup
+
+## PreReqs
+
+- VirtualBox
+- Homebrew
+
+
+### Docker (OSX)
+
+* `boot2docker up` - Kick up boot2docker VM (required each time to use Docker)
---
+++ devops/setup/docker_setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+# Virtualbox is required!
+
+brew update
+brew install boot2docker
+brew install docker
+
+
+boot2docker init
+boot2docker start
+
+echo "---------------------------------------------------------------------------------------"
+echo "Below are boot2docker environment variables that you can add to your .zshrc or .profile"
+echo "---------------------------------------------------------------------------------------"
+boot2docker shellinit
+
+eval "$(boot2docker shellinit)"
+
+docker run hello-world
