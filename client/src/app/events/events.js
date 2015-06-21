a9dc43a22343682363e96e2727d39243a99374e3
Randy Nolen <randy.nolen@geocent.com>
2015-06-21 16:12:15 -0400

    Story #23 - added query to return adverse events; left it commented because it's not running correctly yet; added some code to the html page to display the adverse events if/when I get the query running.

--- client/src/app/events/events.js
+++ client/src/app/events/events.js
@@ -1,9 +1,12 @@
+'use strict';
 
 angular.module('ads.events', ['ads.services.openfda'])
   .controller('AdverseEventsCtrl', function($scope, DrugEventService) {
 
     var query = {
-      'search': "patient.drug.openfda.generic_name:\"promethazine\""
+      'search': 'patient.drug.openfda.generic_name:"promethazine"'
+//      'search': 'patient.drug.openfda.generic_name:"promethazine"+AND+patient.drug.openfda.generic_name:"amitriptyline hydrochloride"',
+//      'count' : 'patient.reaction.reactionmeddrapt.exact'
     };
 
     DrugEventService.get(query, function(data) {
