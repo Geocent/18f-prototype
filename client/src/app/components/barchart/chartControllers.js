2258036608fc5d1feab1c373e0f14e665ca6ef0a
Randy Nolen <randy.nolen@geocent.com>
2015-06-22 11:21:33 -0400

    Story #23 - added service call and data to chart controller

--- client/src/app/components/barchart/chartControllers.js
+++ client/src/app/components/barchart/chartControllers.js
@@ -1,9 +1,27 @@
 'use strict';
 
-var chartControllers = angular.module('chartControllers',['nvd3']);
+var chartControllers = angular.module('chartControllers',['nvd3','ads.services.openfda']);
 
-    chartControllers.controller('BarChartCtrl', function($scope){
+    chartControllers.controller('BarChartCtrl', function($scope, DrugEventService){
 
+        var query = {
+//              'search': 'patient.drug.openfda.generic_name:"promethazine"'
+        	      'search': 'patient.drug.openfda.generic_name:"promethazine" AND ' +
+        	      			'patient.drug.openfda.generic_name:"amitriptyline hydrochloride" AND ' +
+        	      			'patient.drug.openfda.generic_name:"acetaminophen"',
+        	      'count' : 'patient.reaction.reactionmeddrapt.exact',
+        	      'limit' : '20'
+        };
+
+        DrugEventService.get(query, function(data) {
+          console.log(data);
+          $scope.data = [
+             {
+            	 key: 'Adverse Events',
+            	 values: data.results
+             }];
+        });
+            
         $scope.options = {
 	        chart: {
 	            type: 'multiBarHorizontalChart',
@@ -12,7 +30,7 @@ var chartControllers = angular.module('chartControllers',['nvd3']);
                     top: 20,
                     right: 100,
                     bottom: 60,
-                    left: 200
+                    left: 300
                 },
                 x: function(d){return d.term;},
 	            y: function(d){return d.count;},
@@ -32,59 +50,59 @@ var chartControllers = angular.module('chartControllers',['nvd3']);
 	        }
         };
 
-        $scope.data = [
-            {
-                key: 'Cumulative Return',
-                values: [
-                          {
-                             'term': 'NAUSEA',
-                             'count': 7
-                           },
-                           {
-                             'term': 'PAIN',
-                             'count': 6
-                           },
-                           {
-                             'term': 'MYOCARDIAL INFARCTION',
-                             'count': 5
-                           },
-                           {
-                             'term': 'FATIGUE',
-                             'count': 5
-                           },
-                           {
-                             'term': 'DEPRESSION',
-                             'count': 5
-                           },
-                           {
-                             'term': 'VOMITING',
-                             'count': 4
-                           },
-                           {
-                             'term': 'PYREXIA',
-                             'count': 4
-                           },
-                           {
-                             'term': 'INSOMNIA',
-                             'count': 4
-                           },
-                           {
-                             'term': 'HYPERTENSION',
-                             'count': 4
-                           },
-                           {
-                             'term': 'CONSTIPATION',
-                             'count': 4
-                           },
-                           {
-                             'term': 'BACK PAIN',
-                             'count': 4
-                           },
-                           {
-                             'term': 'ANXIETY',
-                             'count': 4
-                           }
-                   ]
-            }
-        ];
+//        $scope.data = [
+//            {
+//                key: 'Cumulative Return',
+//                values: [
+//                          {
+//                             'term': 'NAUSEA',
+//                             'count': 7
+//                           },
+//                           {
+//                             'term': 'PAIN',
+//                             'count': 6
+//                           },
+//                           {
+//                             'term': 'MYOCARDIAL INFARCTION',
+//                             'count': 5
+//                           },
+//                           {
+//                             'term': 'FATIGUE',
+//                             'count': 5
+//                           },
+//                           {
+//                             'term': 'DEPRESSION',
+//                             'count': 5
+//                           },
+//                           {
+//                             'term': 'VOMITING',
+//                             'count': 4
+//                           },
+//                           {
+//                             'term': 'PYREXIA',
+//                             'count': 4
+//                           },
+//                           {
+//                             'term': 'INSOMNIA',
+//                             'count': 4
+//                           },
+//                           {
+//                             'term': 'HYPERTENSION',
+//                             'count': 4
+//                           },
+//                           {
+//                             'term': 'CONSTIPATION',
+//                             'count': 4
+//                           },
+//                           {
+//                             'term': 'BACK PAIN',
+//                             'count': 4
+//                           },
+//                           {
+//                             'term': 'ANXIETY',
+//                             'count': 4
+//                           }
+//                   ]
+//            }
+//        ];
     });
