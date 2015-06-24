c04beaf55c9c9ace965c2bc00fd74b3575be56d1
Aaron <aaron.whitney@gmail.com>
2015-06-24 10:57:38 -0400

    Defect #31: Moves link definitions to controller.

--- client/src/app/components/navbar/navbar.controller.js
+++ client/src/app/components/navbar/navbar.controller.js
@@ -3,5 +3,11 @@
 angular.module('ads.navbar', [])
   .controller('NavbarCtrl', function ($scope) {
     $scope.date = new Date();
+    $scope.nav = {
+        'home': "#/",
+        'about': "#/about",
+        'contact': '#/contact',
+        'source': 'https://github.com/Geocent/18f-prototype'
+    }
   });
 
