b22c0acc286d10973125ce5c4c5e348a574114c7
Aaron <aaron.whitney@gmail.com>
2015-06-29 22:49:36 -0400

    Story #38: Removes unused code.

--- client/src/app/components/piechart/piechart.controller.js
+++ client/src/app/components/piechart/piechart.controller.js
@@ -132,58 +132,6 @@ angular.module('ads.piechart',['nvd3','ads.services.openfda'])
       return sex;
     }
 
-    function loadAgeChartData() {
-
-      $scope.ageChartData = [];
-
-      queryEndpointByAgeRange('< 25', '[0 TO 24]', setAgeChartOptions);
-      queryEndpointByAgeRange('25-49', '[25 TO 49]');
-      queryEndpointByAgeRange('50-74', '[50 TO 74]');
-      queryEndpointByAgeRange('Over 75', '[75 TO 150]');
-
-    }
-
-    function loadWeightChartData() {
-
-      $scope.weightChartData = [];
-
-      queryEndpointByWeightRange('< 100', '[0 TO 99]', setWeightChartOptions);
-      queryEndpointByWeightRange('100-149', '[100 TO 149]');
-      queryEndpointByWeightRange('150-199', '[150 TO 199]');
-      queryEndpointByWeightRange('200-249', '[200 TO 249]');
-      queryEndpointByWeightRange('250-300', '[250 TO 300]');
-      queryEndpointByWeightRange('Over 300', '[300 TO 1000]');
-
-    }
-
-    function queryEndpointByAgeRange(label, searchRange, setChartOptionsCallback) {
-      queryEndpointByRange('patient.patientonsetage', label, searchRange, $scope.ageChartData, setChartOptionsCallback);
-    }
-
-    function queryEndpointByWeightRange(label, searchRange, setChartOptionsCallback) {
-      queryEndpointByRange('patient.patientweight', label, searchRange, $scope.weightChartData, setChartOptionsCallback);
-    }
-
-    function queryEndpointByRange(searchField, label, searchRange, chartData, setChartOptionsCallback) {
-      var searchCriteria = ' AND ' + searchField + ':' + searchRange;
-      if ($attrs.detailSection) {
-        searchCriteria = searchCriteria + ' AND patient.reaction.reactionmeddrapt:' + $scope.symptomName;
-      }
-      MedicationsSearchService.query($scope.adverseEvents, searchCriteria, 'receivedateformat', null, function(data) {
-        var i, result;
-        for (i = 0; i < data.results.length; ++i) {
-          result = data.results[i];
-          chartData.push({
-            key: label,
-            y: result.count
-          });
-        }
-        if (setChartOptionsCallback) {
-          setChartOptionsCallback();
-        }
-      });
-    }
-
     function loadAggregatedAgeChartData() {
       var ranges = [
         {start: 0, end: 24, label: '< 25', total: 0},
